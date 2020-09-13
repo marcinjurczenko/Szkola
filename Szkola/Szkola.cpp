2ed0b25e542cf17b024e5f5134f0a0e63ee3dbe1
jurczenkomarcin <jurczenkomarcin@gmail.com>
2020-09-13 14:33:45 +0200

    ogólne poprawienie programu dodanie działan matematycznych

--- Szkola/Szkola.cpp
+++ Szkola/Szkola.cpp
@@ -4,29 +4,127 @@ using namespace std;
 
 int main()
 {
+    Przywitanie();
+
     while (true)
     {
         int wyjscie = Menu();
 
-        if (wyjscie == 2) 
+        if (wyjscie == 0) 
         {
             break;
         }
 
-        Przywitanie();
+        if (wyjscie == 1)
+        {
+            cout << "podaj pierwsza liczbe: ";
+            int liczba1;
+            cin >> liczba1;
+            cout << "podaj druga liczbe: ";
+            int liczba2;
+            cin >> liczba2;
 
-        Czyszczenie();
+
+            int wynik = Dodawanie(liczba1, liczba2);
+
+            cout << "wynik dodawania to: " << wynik << endl << endl;
+
+
+            Czyszczenie();
+        }
+
+        if (wyjscie == 2)
+        {
+            cout << "podaj liczbe od ktorej odejmujesz: ";
+            int liczba1;
+            cin >> liczba1;
+            cout << "podaj liczbe ktora chcesz odjac: ";
+            int liczba2;
+            cin >> liczba2;
+
+
+            int wynik = Odejmowanie(liczba1, liczba2);
+
+            cout << "wynik odejmowania to: " << wynik << endl << endl;
+
+
+            Czyszczenie();
+        }
+
+        if (wyjscie == 3)
+        {
+            cout << "podaj pierwsza liczbe: ";
+            int liczba1;
+            cin >> liczba1;
+            cout << "podaj druga liczbe: ";
+            int liczba2;
+            cin >> liczba2;
+
+
+            int wynik = Mnozenie(liczba1, liczba2);
+
+            cout << "wynik mnozenia to: " << wynik << endl << endl;
+
+
+            Czyszczenie();
+        }
+
+        if (wyjscie == 4)
+        {
+            cout << "podaj liczbe ktora chcesz podzielic: ";
+            int liczba1;
+            cin >> liczba1;
+            cout << "podaj liczbe przez ktora dzielisz: ";
+            int liczba2;
+            cin >> liczba2;
+
+
+            int wynik = Mnozenie(liczba1, liczba2);
+
+            cout << "wynik dzielenia to: " << wynik << endl << endl;
+
+
+            Czyszczenie();
+        }
+
     }
 }
 
+int Dodawanie(int liczba1, int liczba2)
+{
+    int wynik = liczba1 + liczba2;
+    return wynik;
+}
+
+int Odejmowanie(int liczba1, int liczba2)
+{
+    int wynik = liczba1 - liczba2;
+    return wynik;
+}
+
+int Mnozenie(int liczba1, int liczba2)
+{
+    int wynik = liczba1 * liczba2;
+    return wynik;
+}
+
+int Dzielenie(int liczba1, int liczba2)
+{
+    int wynik = liczba1 / liczba2;
+    return wynik;
+}
 
 
 int Menu()
 {
     int wyjscie;
-    cout << "MENU :\n";
-    cout << "cyfra - przywitanie\n";
-    cout << "2 - wyjscie\n";
+    cout << "MENU:\n";
+    cout << "*Dodawanie - 1\n";
+    cout << "*Odejmowanie - 2\n";
+    cout << "*Mnozenie - 3\n";
+    cout << "*Dzielenie - 4\n";
+    cout << "*Wyjscie - 0\n";
+    
     cin >> wyjscie;
 
     return wyjscie;
@@ -35,14 +133,14 @@ int Menu()
 void Przywitanie()
 {
     string imie;
-    cout << "Podaj swoje Imie : ";
+    cout << "Podaj swoje imie: ";
     cin >> imie;
-    cout << "Twoje imie to : " << imie << endl;
+    cout << endl << "Witaj " << imie << "!" << endl << endl;
 }
 
 void Czyszczenie()
 {
-    cout << "press any key to exit...";
+    cout << "wpisz dowolny znak i nacisnij enter...";
     char temp;
     cin >> temp;
     system("cls");
--- Szkola/Szkola.h
+++ Szkola/Szkola.h
@@ -2,6 +2,16 @@
 
 void Przywitanie();
 
+int main();
+
 int Menu();
 
 void Czyszczenie();
+
+int Dodawanie(int liczba1, int liczba2);
+
+int Odejmowanie(int liczba1, int liczba2);
+
+int Mnozenie(int liczba1, int liczba2);
+
+int Dzielenie(int liczba1, int liczba2);
