3eaa8a540536306ac1074cb556d931b92f0ba234
jurczenkomarcin <jurczenkomarcin@gmail.com>
2020-09-25 18:01:32 +0200

    petla for

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -6,23 +6,21 @@ namespace Csharp
     {
         static void Main(string[] args)
         {
-
-            do
+            for (;;)
             {
                 Console.WriteLine("Hello World!");
                 Console.WriteLine("Podaj imie");
                 string imie = Console.ReadLine();
                 Console.WriteLine("Witaj " + imie);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+
                 Console.ReadKey();
                 Console.Clear();
-            } while (true);
-                
-            
-            
-
- 
+            }
         }
     }
-
-
 }
