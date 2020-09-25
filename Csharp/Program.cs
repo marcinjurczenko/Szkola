bd924c5a4b1fe4e50937766b9f64250269bbc2bc
jurczenkomarcin <jurczenkomarcin@gmail.com>
2020-09-25 17:51:19 +0200

    Przeniesienie klasy do osobnego plkiku

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -19,36 +19,4 @@ namespace Csharp
     }
 
 
-    class Pies
-    {
-        public Pies() : this("pies")
-        {
-        }
-
-        public Pies(string _imie)
-        {
-            ilosc++;
-            imie = _imie;
-        }
-
-        public static int ilosc;
-        private string imie;
-
-        public void PodajImie(string imie)
-        {
-            Console.WriteLine(imie);
-        }
-
-        public static void WypiszTekst(string tekst)
-        {
-            Console.WriteLine("Hello " + tekst);
-        }
-
-        public void Przywitanie()
-        {
-            Console.WriteLine("moje imie to " + imie);
-        }
-    }
-
-
 }
